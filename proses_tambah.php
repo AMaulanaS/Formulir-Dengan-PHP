969881c75e1a3170a1f5334aec9dadd7a3d94e7f
Aku Alan Favoritmu <aditymaulino@gmail.com>
2022-09-09 13:47:52 +0700

    Update proses_tambah.php

--- proses_tambah.php
+++ proses_tambah.php
@@ -3,9 +3,10 @@
 include 'koneksi.php';
 
 	// membuat variabel untuk menampung data dari form
-  $nama_lengkap  = $_POST['nama_lengkap'];
-  $departemen    = $_POST['departemen'];
+  $nama_lengkap   = $_POST['nama_lengkap'];
+  $departemen     = $_POST['departemen'];
   $jabatan    = $_POST['jabatan'];
+  $alamat    = $_POST['alamat'];
   $gambar_qrcode = $_FILES['gambar_qrcode']['name'];
 
 
@@ -20,7 +21,7 @@ if($gambar_qrcode != "") {
         if(in_array($ekstensi, $ekstensi_diperbolehkan) === true)  {     
                 move_uploaded_file($file_tmp, 'gambar/'.$nama_gambar_baru); //memindah file gambar ke folder gambar
                   // jalankan query INSERT untuk menambah data ke database pastikan sesuai urutan (id tidak perlu karena dibikin otomatis)
-                  $query = "INSERT INTO qrcode (nama_lengkap, departemen, jabatan, gambar_qrcode) VALUES ('$nama_lengkap', '$departemen', '$jabatan', '$nama_gambar_baru')";
+                  $query = "INSERT INTO qrcode (nama_lengkap, departemen, jabatan, alamat, gambar_qrcode) VALUES ('$nama_lengkap', '$departemen', '$jabatan', '$alamat', '$nama_gambar_baru')";
                   $result = mysqli_query($koneksi, $query);
                   // periska query apakah ada error
                   if(!$result){
@@ -34,10 +35,10 @@ if($gambar_qrcode != "") {
 
             } else {     
              //jika file ekstensi tidak jpg dan png maka alert ini yang tampil
-                echo "<script>alert('Ekstensi gambar yang boleh hanya jpg atau png.');window.location='tambah_produk.php';</script>";
+                echo "<script>alert('Ekstensi gambar yang boleh hanya jpg atau png.');window.location='tambah.php';</script>";
             }
 } else {
-   $query = "INSERT INTO qrcode (nama_lengkap, departemen, jabatan, gambar_qrcode) VALUES ('$nama_produk', '$deskripsi', '$harga_beli', '$harga_jual', null)";
+   $query = "INSERT INTO qrcode (nama_lengkap, departemen, jabatan, alamat, gambar_qrcode) VALUES ('$nama_lengkap', '$departemen', '$jabatan', '$alamat', null)";
                   $result = mysqli_query($koneksi, $query);
                   // periska query apakah ada error
                   if(!$result){
