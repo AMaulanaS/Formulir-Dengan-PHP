8be4af072c40a2ec384619c4849850e3c8b435cb
Aku Alan Favoritmu <aditymaulino@gmail.com>
2022-09-09 13:31:28 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,26 +1,44 @@
  <?php
+  // memanggil file koneksi.php untuk membuat koneksi
 include 'koneksi.php';
+
+  // mengecek apakah di url ada nilai GET id
   if (isset($_GET['id'])) {
+    // ambil nilai id dari url dan disimpan dalam variabel $id
     $id = ($_GET["id"]);
+
+    // menampilkan data dari database yang mempunyai id=$id
     $query = "SELECT * FROM qrcode WHERE id='$id'";
     $result = mysqli_query($koneksi, $query);
+    // jika data gagal diambil maka akan tampil error berikut
     if(!$result){
       die ("Query Error: ".mysqli_errno($koneksi).
          " - ".mysqli_error($koneksi));
     }
     // mengambil data dari database
     $data = mysqli_fetch_assoc($result);
+      // apabila data tidak ada pada database maka akan dijalankan perintah ini
        if (!count($data)) {
           echo "<script>alert('Data tidak ditemukan pada database');window.location='index.php';</script>";
        }
   } else {
+    // apabila tidak ada data GET id pada akan di redirect ke index.php
     echo "<script>alert('Masukkan data id.');window.location='index.php';</script>";
   }         
   ?>
+
 <!DOCTYPE html>
-<html>
-  <head>
-    <style type="text/css">
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Formulir CWA Pusat</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="css/styles.css">
+<script type="text/javascript" src="js/qrcode.js"></script>
+<style type="text/css">
       * {
         font-family: "Trebuchet MS";
       }
@@ -61,15 +79,15 @@ include 'koneksi.php';
       padding: 20px;
       margin-left: auto;
       margin-right: auto;
-      background: #ededed;
+      background: transparent;
     }
     </style>
   </head>
   <body>
       <center>
-        <h1>Edit <?php echo $data['nama_lengkap']; ?></h1>
+        <h1>Edit Data <?php echo $data['nama_lengkap']; ?></h1>
       <center>
-      <form method="POST" action="edit.php" enctype="multipart/form-data" >
+      <form method="POST" action="proses_edit.php" enctype="multipart/form-data" >
       <section class="base">
         <!-- menampung nilai id produk yang akan di edit -->
         <input name="id" value="<?php echo $data['id']; ?>"  hidden />
@@ -86,7 +104,11 @@ include 'koneksi.php';
          <input type="text" name="jabatan" required=""value="<?php echo $data['jabatan']; ?>" />
         </div>
         <div>
-          <label>Gambar QRcode</label>
+          <label>Alamat</label>
+         <input type="text" name="alamat" required="" value="<?php echo $data['alamat']; ?>"/>
+        </div>
+        <div>
+          <label>Gambar QR</label>
           <img src="gambar/<?php echo $data['gambar_qrcode']; ?>" style="width: 120px;float: left;margin-bottom: 5px;">
           <input type="file" name="gambar_qrcode" />
           <i style="float: left;font-size: 11px;color: red">Abaikan jika tidak merubah gambar</i>
